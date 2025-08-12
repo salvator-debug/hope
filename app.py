31b05986faed5af63e043007ad9f58924ec73193
haker <nikkenmilka@gmail.com>
2025-08-12 22:50:31 +0300

    Update project

--- app.py
+++ app.py
@@ -472,5 +472,6 @@ def mode3():
 
     return render_template('mode3_content.html', systems=systems, results=results)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
